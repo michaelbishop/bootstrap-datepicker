45f703a82d533933b191197c7d402909707c810e
Michael Bishop <michael@michaelsplace.net>
2013-07-22 20:38:58 -0700

    fix typos

--- package.js
+++ package.js
@@ -3,8 +3,8 @@ Package.describe({
 });
 
 Package.on_use(function(api) {
-	api.add_files('js/bootstrap-datetimepicker.js', 'client');
-	api.add_files('css/datetimepicker.css', 'client');
+	api.add_files('js/bootstrap-datepicker.js', 'client');
+	api.add_files('css/datepicker.css', 'client');
 
 	api.use('less', 'client');
 });
